cb246d6f657f4e0371159b1d67c7a9be7ab65c88
7apislaz <belltech0210@gmail.com>
2022-10-31 18:30:02 +0900

    [Update] main.dart 라우터, 스플래시스크린 적용 및 디버그배너 해제

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vision21tech_smartkiosk/theme.dart';
+import 'routes.dart';
+import 'screens/splash/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SmartKiosk',
       theme: theme(),
+      initialRoute: SplashScreen.routeName,
+      routes: route,
     );
   }
 }
