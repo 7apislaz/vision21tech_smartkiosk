6e52fb333cc7e56c00b4ec2bbf848ee89e8c3d85
7apislaz <belltech0210@gmail.com>
2022-11-07 18:09:25 +0900

    [Update] 버튼모듈 추가, 메인페이지 레이아웃 완성

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// import 'package:vision21tech_smartkiosk/constants.dart';
+import 'package:vision21tech_smartkiosk/module/button.dart';
 import 'package:vision21tech_smartkiosk/screens/splash/splash_screen.dart';
 // import 'package:vision21tech_smartkiosk/theme.dart';
-// import 'package:vision21tech_smartkiosk/constants.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String routeName = "/main";
@@ -22,42 +23,47 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         ),
       ),
       child: Scaffold(
-          appBar: AppBar(
-              // title:
-              // Text('스마트 어린이집', style: textTheme().headline1),
+        appBar: AppBar(
+          // title:
+          // Text('스마트 어린이집', style: textTheme().headline1),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Expanded(
+          flex: 2,
+          child: Container(
+            margin: EdgeInsets.only(top: 50, left: 100, right: 100),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/kindergarten.png'),
+                fit: BoxFit.contain,
               ),
-          body: Column(
-
-            crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+          ),
+        ),
+        Padding(padding: const EdgeInsets.only(top:60, left: 100.0, right: 100.0, bottom: 200.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/kindergarten.png'),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
+              Kiosk_Button(
+                  text: '키 재기',
+                  onPressed: () {
+                    Get.to(SplashScreen());
+                  },
               ),
-              Expanded(
-                flex: 1,
-                child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(onPressed: () {
-                      Get.off(SplashScreen());},
-                      child: Text('키 재기'),
-                    ),
-                    ElevatedButton(onPressed: () {
-                      Get.to(SplashScreen());},
-                      child: Text('감정 수집'),
-                    ),
-                  ],
-                ),
+              SizedBox(height: 70),
+              Kiosk_Button(
+                  text: '감정 수집',
+                  onPressed: () {
+                    Get.to(SplashScreen());
+                  },
               ),
             ],
-          )),
+          ),
+        ),
+      ],
+    )),
     );
   }
 }
