e1b14d4b15e79dfa83c00328cf78848afab51893
7apislaz <belltech0210@gmail.com>
2022-11-08 20:16:48 +0900

    [Update] 새 페이지 루트 추가

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:vision21tech_smartkiosk/screens/height_measurement_screen.dart';
 import 'screens/splash/splash_screen.dart';
 import 'screens/welcome_screen.dart';
 
 final Map<String, WidgetBuilder> route = {
   SplashScreen.routeName: (context) => SplashScreen(),
   WelcomeScreen.routeName: (context) => WelcomeScreen(),
+  HeightMeasure.routeName: (context) => HeightMeasure(),
 };
