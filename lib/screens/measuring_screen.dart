0acf57ca0395c09c176c0e408da07c7fc20bf797
7apislaz <belltech0210@gmail.com>
2022-11-09 18:38:39 +0900

    [Update] 측정 화면 페이지

---
+++ lib/screens/measuring_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vision21tech_smartkiosk/screens/welcome_screen.dart';
+// import 'package:vision21tech_smartkiosk/module/button.dart';
+// import '../constants.dart';
+
+class MesuringScreen extends StatefulWidget {
+  static String routeName = "/mesureerror";
+
+  @override
+  _MesuringScreenState createState() => _MesuringScreenState();
+}
+
+class _MesuringScreenState extends State<MesuringScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage('assets/images/background.png'),
+        ),
+      ),
+      child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_rounded, size: 60,),
+              onPressed: () {
+                Get.off(WelcomeScreen());
+              },
+            ),
+            // title:
+            // Text('스마트 어린이집', style: textTheme().headline1),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  margin: EdgeInsets.only(top: 130, left: 100, right: 100),
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/HeightKid.png'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(padding: const EdgeInsets.only(top:80, left: 100.0, right: 100.0, bottom: 180.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("측정기 위에 올라가 주세요", textScaleFactor: 3.5,),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
