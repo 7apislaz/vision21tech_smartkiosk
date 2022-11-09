fd1524f5f388787307e1317a5d6b88ec6c481fc0
7apislaz <belltech0210@gmail.com>
2022-11-09 18:38:58 +0900

    [Update] 측정 화면 페이지 및 측정기 연결 오류 페이지 루트 추가

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vision21tech_smartkiosk/screens/height_measurement_screen.dart';
+import 'package:vision21tech_smartkiosk/screens/measurement_error.dart';
+import 'package:vision21tech_smartkiosk/screens/measuring_screen.dart';
 import 'screens/splash/splash_screen.dart';
 import 'screens/welcome_screen.dart';
 
@@ -7,4 +9,6 @@ final Map<String, WidgetBuilder> route = {
   SplashScreen.routeName: (context) => SplashScreen(),
   WelcomeScreen.routeName: (context) => WelcomeScreen(),
   HeightMeasure.routeName: (context) => HeightMeasure(),
+  MesureErrorScreen.routeName: (context) => MesureErrorScreen(),
+  MesuringScreen.routeName: (context) => MesuringScreen(),
 };
