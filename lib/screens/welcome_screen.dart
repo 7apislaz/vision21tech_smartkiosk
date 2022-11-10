1b9178543e0afb9baa5cf3cafd4472cdb4de81bb
7apislaz <belltech0210@gmail.com>
2022-11-10 18:32:33 +0900

    [Update] 세팅 페이지 버튼 완성

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:vision21tech_smartkiosk/constants.dart';
 import 'package:vision21tech_smartkiosk/module/button.dart';
+import 'package:vision21tech_smartkiosk/screens/kiosk_setting_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/splash/splash_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/height_measurement_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/measuring_screen.dart';
@@ -15,6 +17,7 @@ class WelcomeScreen extends StatefulWidget {
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
+  int touchCount = 0;
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
@@ -27,60 +30,85 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            InkWell(
-              onDoubleTap: (){
-                print("굿");
+            SizedBox(height: 140),
+            GestureDetector(
+              onTap: (){
+                touchCount++;
+                if(touchCount == 3) {
+                  Get.to(() => KioskSettingScreen());
+                }
               },
+              behavior: HitTestBehavior.translucent,
               child: Container(
-                  padding: EdgeInsets.only(left: 800, bottom: 50),
-                  child: Text("")
+                alignment: Alignment.topLeft,
+                width: 720, height: 80,
               ),
             ),
-          Expanded(
-          flex: 2,
-          child: Container(
-            margin: EdgeInsets.only(top: 130, left: 100, right: 100),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/kindergarten.png'),
-                fit: BoxFit.contain,
+            Expanded(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.only(left: 100, right: 100),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/kindergarten.png'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Padding(padding: const EdgeInsets.only(top:60, left: 100.0, right: 100.0, bottom: 180.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Kiosk_Button(
-                  text: '키 재기',
-                  textScale: 5,
-                  maxiSize: Size(500, 160),
-                  miniSize: Size(500, 160),
-                  buttonColor: kOrangeButtonColor,
-                  textStyle: Theme.of(context).textTheme.bodyText1,
-                  onPressed: () {
-                    Get.to(HeightMeasure());
-                  },
+            Padding(padding: const EdgeInsets.only(top:60, left: 100.0, right: 100.0, bottom: 180.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Kiosk_Button(
+                    text: '키 재기',
+                    textScale: 5,
+                    maxiSize: Size(500, 160),
+                    miniSize: Size(500, 160),
+                    buttonColor: kOrangeButtonColor,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                    onPressed: () {
+                      Get.to(() => HeightMeasure());
+                    },
+                  ),
+                  SizedBox(height: 70),
+                  Kiosk_Button(
+                    text: '감정 수집',
+                    textScale: 5,
+                    maxiSize: Size(500, 160),
+                    miniSize: Size(500, 160),
+                    buttonColor: kOrangeButtonColor,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                    onPressed: () {
+                      Get.to(MesuringScreen());
+                    },
+                  ),
+                ],
               ),
-              SizedBox(height: 70),
-              Kiosk_Button(
-                  text: '감정 수집',
-                  textScale: 5,
-                  maxiSize: Size(500, 160),
-                  miniSize: Size(500, 160),
-                  buttonColor: kOrangeButtonColor,
-                  textStyle: Theme.of(context).textTheme.bodyText1,
-                  onPressed: () {
-                    Get.to(MesuringScreen());
-                  },
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
-    ),
       ),
     );
   }
 }
+
+
+// Widget buildSettingButton(VoidCallback onTap) {
+//   var touchCount;
+//   return GestureDetector(
+//     onTap: (){
+//       touchCount++;
+//       if(touchCount == 3) {
+//         print("굿");
+//       }
+//     },
+//     behavior: HitTestBehavior.translucent,
+//     child: Container(
+//       padding: EdgeInsets.only(right: 400, bottom: 50),
+//       width: 400, height: 50,
+//       color: kGrayButtonColor,
+//     ),
+//   );
+// }
+//
