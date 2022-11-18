27af93290783a7370d14d3a5de304c4630eabec0
7apislaz <belltech0210@gmail.com>
2022-11-18 18:39:54 +0900

    [Update] 메인 페이지 버튼 루트 수정

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:vision21tech_smartkiosk/constants.dart';
 import 'package:vision21tech_smartkiosk/module/button.dart';
+import 'package:vision21tech_smartkiosk/screens/emotion_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/kiosk_setting_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/splash/splash_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/height_measurement_screen.dart';
@@ -80,7 +81,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                     buttonColor: kOrangeButtonColor,
                     textStyle: Theme.of(context).textTheme.bodyText1,
                     onPressed: () {
-                      Get.to(MesuringScreen());
+                      Get.to(EmotionScreen());
                     },
                   ),
                 ],
