19ecdef82154e8c5bfb92d152f0d7080c4c69c95
7apislaz <belltech0210@gmail.com>
2022-11-25 11:35:34 +0900

    [Update] 페이지 루트 추가

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,10 +3,12 @@ import 'package:vision21tech_smartkiosk/screens/camera_error.dart';
 import 'package:vision21tech_smartkiosk/screens/data_error.dart';
 import 'package:vision21tech_smartkiosk/screens/emotion_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/height_measurement_screen.dart';
+import 'package:vision21tech_smartkiosk/screens/kid_list_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/kiosk_setting_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/measurement_error.dart';
 import 'package:vision21tech_smartkiosk/screens/measuring_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/network_error.dart';
+import 'package:vision21tech_smartkiosk/screens/playing_screen.dart';
 import 'screens/splash/splash_screen.dart';
 import 'screens/welcome_screen.dart';
 
@@ -21,4 +23,6 @@ final Map<String, WidgetBuilder> route = {
   DataErrorScreen.routeName: (context) => DataErrorScreen(),
   CameraErrorScreen.routeName: (context) => CameraErrorScreen(),
   EmotionScreen.routeName: (context) => EmotionScreen(),
+  PlayingScreen.routeName: (context) => PlayingScreen(),
+  KidListScreen.routeName: (context) => KidListScreen(),
 };
