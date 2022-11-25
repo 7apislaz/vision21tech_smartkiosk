1b2ab5130bbe420886f19d8fe0647c158d1c3f17
7apislaz <belltech0210@gmail.com>
2022-11-25 19:30:01 +0900

    [Update] 감정 선택화면 WillPopScope 구현

--- lib/screens/emotion_screen.dart
+++ lib/screens/emotion_screen.dart
@@ -3,7 +3,9 @@ import 'package:get/get.dart';
 import 'package:vision21tech_smartkiosk/constants.dart';
 import 'package:vision21tech_smartkiosk/module/button.dart';
 import 'package:vision21tech_smartkiosk/screens/kiosk_setting_screen.dart';
+import 'package:vision21tech_smartkiosk/screens/playing_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/welcome_screen.dart';
+import 'package:vision21tech_smartkiosk/mydata.dart';
 
 class EmotionScreen extends StatefulWidget {
   static String routeName = "/emotion";
@@ -13,198 +15,256 @@ class EmotionScreen extends StatefulWidget {
 }
 
 class _EmotionScreenState extends State<EmotionScreen> {
+  final MyData myData = Get.put(MyData(
+    emotion: '',
+  ));
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.fill,
-          image: AssetImage('assets/images/background.png'),
-        ),
-      ),
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_rounded, size: 60,),
-            onPressed: () {
-              Get.off(WelcomeScreen());
-            },
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/background.png'),
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-          Padding(padding: const EdgeInsets.only(top:80, left: 100.0, right: 100.0),
-            child: Column(
-              children: [
-                Text("오늘 어린이집에 오기 전에", textScaleFactor: 3.2,),
-                SizedBox(height: 10),
-                Text("집에서 너의 마음은 어땠니?", textScaleFactor: 3.2,),
-                SizedBox(height: 40,),
-              ],
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back_rounded,
+                size: 60,
+              ),
+              onPressed: () {
+                Get.off(WelcomeScreen());
+              },
             ),
           ),
-          Padding(padding: EdgeInsets.only(bottom: 10),
-            child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          body: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                InkWell(
-                  child: Container(
-                    padding: EdgeInsets.only(bottom: 10),
-                    alignment: AlignmentDirectional.bottomCenter,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFFFCB64),
-                      image: DecorationImage(
-                        fit: BoxFit.scaleDown,
-                        scale: 2.4,
-                        image: AssetImage('assets/images/happyboy.png'),
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 80, left: 100.0, right: 100.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "오늘 어린이집에 오기 전에",
+                      textScaleFactor: 3.2,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "집에서 너의 마음은 어땠니?",
+                      textScaleFactor: 3.2,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Get.to(() => PlayingScreen(), arguments: [
+                          myData.emotion = "happy",
+                        ]);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 10),
+                        alignment: AlignmentDirectional.bottomCenter,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFFFCB64),
+                          image: DecorationImage(
+                            fit: BoxFit.scaleDown,
+                            scale: 2.4,
+                            image: AssetImage('assets/images/happyboy.png'),
+                          ),
+                        ),
+                        child: Text(
+                          "행복",
+                          textAlign: TextAlign.center,
+                          textScaleFactor: 1.8,
+                          style: TextStyle(color: kWhiteFontColor),
+                        ),
+                        width: 300,
+                        height: 280,
                       ),
                     ),
-                    child: Text(
-                      "행복",
-                      textAlign: TextAlign.center,
-                      textScaleFactor: 1.8,
-                      style: TextStyle(color: kWhiteFontColor),
+                    SizedBox(
+                      width: 10,
                     ),
-                    width: 300,
-                    height: 280,
-                  ),
-                ),
-                SizedBox(width: 10,),
-                InkWell(
-                  child: Container(
-                    padding: EdgeInsets.only(bottom: 10),
-                    alignment: AlignmentDirectional.bottomCenter,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFFFCB64),
-                      image: DecorationImage(
-                        fit: BoxFit.scaleDown,
-                        scale: 2.4,
-                        image: AssetImage('assets/images/excitedBoy.png'),
+                    InkWell(
+                      onTap: () {
+                        Get.to(() => PlayingScreen(), arguments: [
+                          myData.emotion = "excited",
+                        ]);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 10),
+                        alignment: AlignmentDirectional.bottomCenter,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFFFCB64),
+                          image: DecorationImage(
+                            fit: BoxFit.scaleDown,
+                            scale: 2.4,
+                            image: AssetImage('assets/images/excitedBoy.png'),
+                          ),
+                        ),
+                        child: Text(
+                          "신남",
+                          textAlign: TextAlign.center,
+                          textScaleFactor: 1.8,
+                          style: TextStyle(color: kWhiteFontColor),
+                        ),
+                        width: 300,
+                        height: 280,
                       ),
                     ),
-                    child: Text(
-                      "신남",
-                      textAlign: TextAlign.center,
-                      textScaleFactor: 1.8,
-                      style: TextStyle(color: kWhiteFontColor),
-                    ),
-                    width: 300,
-                    height: 280,
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Get.to(() => PlayingScreen(), arguments: [
+                          myData.emotion = "sad",
+                        ]);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 10),
+                        alignment: AlignmentDirectional.bottomCenter,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF3A200),
+                          image: DecorationImage(
+                            fit: BoxFit.scaleDown,
+                            scale: 2.4,
+                            image: AssetImage('assets/images/sadBoy.png'),
+                          ),
+                        ),
+                        child: Text(
+                          "우울",
+                          textAlign: TextAlign.center,
+                          textScaleFactor: 1.8,
+                          style: TextStyle(color: kWhiteFontColor),
+                        ),
+                        width: 300,
+                        height: 280,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Get.to(() => PlayingScreen(), arguments: [
+                          myData.emotion = "angry",
+                        ]);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 10),
+                        alignment: AlignmentDirectional.bottomCenter,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF3A200),
+                          image: DecorationImage(
+                            fit: BoxFit.scaleDown,
+                            scale: 2.4,
+                            image: AssetImage('assets/images/angryBoy.png'),
+                          ),
+                        ),
+                        child: Text(
+                          "화남",
+                          textAlign: TextAlign.center,
+                          textScaleFactor: 1.8,
+                          style: TextStyle(color: kWhiteFontColor),
+                        ),
+                        width: 300,
+                        height: 280,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Get.to(() => PlayingScreen(), arguments: [
+                          myData.emotion = "worry",
+                        ]);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 10),
+                        alignment: AlignmentDirectional.bottomCenter,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF4684FF),
+                          image: DecorationImage(
+                            fit: BoxFit.scaleDown,
+                            scale: 2.4,
+                            image: AssetImage('assets/images/worryBoy.png'),
+                          ),
+                        ),
+                        child: Text(
+                          "걱정",
+                          textAlign: TextAlign.center,
+                          textScaleFactor: 1.8,
+                          style: TextStyle(color: kWhiteFontColor),
+                        ),
+                        width: 300,
+                        height: 280,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Get.to(() => PlayingScreen(), arguments: [
+                          myData.emotion = "lonely",
+                        ]);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 10),
+                        alignment: AlignmentDirectional.bottomCenter,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF4684FF),
+                          image: DecorationImage(
+                            fit: BoxFit.scaleDown,
+                            scale: 2.4,
+                            image: AssetImage('assets/images/lonelyBoy.png'),
+                          ),
+                        ),
+                        child: Text(
+                          "외로움",
+                          textAlign: TextAlign.center,
+                          textScaleFactor: 1.8,
+                          style: TextStyle(color: kWhiteFontColor),
+                        ),
+                        width: 300,
+                        height: 280,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-            Padding(padding: EdgeInsets.only(bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  InkWell(
-                    child: Container(
-                      padding: EdgeInsets.only(bottom: 10),
-                      alignment: AlignmentDirectional.bottomCenter,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFF3A200),
-                        shape: ,
-                        image: DecorationImage(
-                          fit: BoxFit.scaleDown,
-                          scale: 2.4,
-                          image: AssetImage('assets/images/sadBoy.png'),
-                        ),
-                      ),
-                      child: Text(
-                        "우울",
-                        textAlign: TextAlign.center,
-                        textScaleFactor: 1.8,
-                        style: TextStyle(color: kWhiteFontColor),
-                      ),
-                      width: 300,
-                      height: 280,
-                    ),
-                  ),
-                  SizedBox(width: 10,),
-                  InkWell(
-                    child: Container(
-                      padding: EdgeInsets.only(bottom: 10),
-                      alignment: AlignmentDirectional.bottomCenter,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFF3A200),
-                        image: DecorationImage(
-                          fit: BoxFit.scaleDown,
-                          scale: 2.4,
-                          image: AssetImage('assets/images/angryBoy.png'),
-                        ),
-                      ),
-                      child: Text(
-                        "화남",
-                        textAlign: TextAlign.center,
-                        textScaleFactor: 1.8,
-                        style: TextStyle(color: kWhiteFontColor),
-                      ),
-                      width: 300,
-                      height: 280,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  InkWell(
-                    child: Container(
-                      padding: EdgeInsets.only(bottom: 10),
-                      alignment: AlignmentDirectional.bottomCenter,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF4684FF),
-                        image: DecorationImage(
-                          fit: BoxFit.scaleDown,
-                          scale: 2.4,
-                          image: AssetImage('assets/images/worryBoy.png'),
-                        ),
-                      ),
-                      child: Text(
-                        "걱정",
-                        textAlign: TextAlign.center,
-                        textScaleFactor: 1.8,
-                        style: TextStyle(color: kWhiteFontColor),
-                      ),
-                      width: 300,
-                      height: 280,
-                    ),
-                  ),
-                  SizedBox(width: 10,),
-                  InkWell(
-                    child: Container(
-                      padding: EdgeInsets.only(bottom: 10),
-                      alignment: AlignmentDirectional.bottomCenter,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF4684FF),
-                        image: DecorationImage(
-                          fit: BoxFit.scaleDown,
-                          scale: 2.4,
-                          image: AssetImage('assets/images/lonelyBoy.png'),
-                        ),
-                      ),
-                      child: Text(
-                        "외로움",
-                        textAlign: TextAlign.center,
-                        textScaleFactor: 1.8,
-                        style: TextStyle(color: kWhiteFontColor),
-                      ),
-                      width: 300,
-                      height: 280,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
         ),
       ),
     );
