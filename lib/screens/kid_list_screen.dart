4961da83b41447989a912fe748f341085dddbfea
7apislaz <belltech0210@gmail.com>
2022-11-28 11:43:34 +0900

    [Update] 아이 목록 Dialog 구현

--- lib/screens/kid_list_screen.dart
+++ lib/screens/kid_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vision21tech_smartkiosk/constants.dart';
-import 'package:vision21tech_smartkiosk/screens/playing_screen.dart';
+import 'package:vision21tech_smartkiosk/screens/height_measurement_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/welcome_screen.dart';
 
 class KidListScreen extends StatefulWidget {
@@ -14,6 +14,7 @@ class KidListScreen extends StatefulWidget {
 class _KidListScreenState extends State<KidListScreen> {
   final List<String> _items = List.generate(30, (index) => 'Item ${index + 1}');
 
+  var kidName = "";
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -63,7 +64,81 @@ class _KidListScreenState extends State<KidListScreen> {
                     child: Center(
                       child: InkWell(
                         onTap: () {
-                          Get.to(() => PlayingScreen());
+                          kidName = _items[index].split(' ')[1];
+                          showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                titlePadding: EdgeInsets.only(
+                                    top: 100, bottom: 60, right: 60, left: 60),
+                                contentPadding:
+                                EdgeInsets.only(right: 60, left: 60),
+                                actionsPadding: EdgeInsets.only(
+                                    top: 60, bottom: 100, right: 60, left: 60),
+                                actionsAlignment: MainAxisAlignment.center,
+                                title: Text('아이 $kidName 친구야 안녕!',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontFamily: 'Godo',
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 50,
+                                      color: kDarkFontColor,
+                                    )),
+                                content: SingleChildScrollView(
+                                  child: InkWell(
+                                    child: Container(
+                                      padding: EdgeInsets.only(bottom: 10),
+                                      alignment:
+                                      AlignmentDirectional.bottomCenter,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          fit: BoxFit.contain,
+                                          image: AssetImage(
+                                              'assets/images/KioskAndKid.png'),
+                                        ),
+                                      ),
+                                      width: 400,
+                                      height: 400,
+                                    ),
+                                  ),
+                                ),
+                                actions: [
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: kOrangeButtonColor,
+                                      maximumSize: Size(260, 100),
+                                      minimumSize: Size(260, 100),
+                                    ),
+                                    onPressed: () {
+                                      Get.to(() => HeightMeasure());
+                                    },
+                                    child: Text(
+                                      "맞아요!",
+                                      style: TextStyle(fontSize: 30),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 40,
+                                  ),
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: kGrayButtonColor,
+                                      maximumSize: Size(260, 100),
+                                      minimumSize: Size(260, 100),
+                                    ),
+                                    onPressed: () {
+                                      Get.back();
+                                    },
+                                    child: Text(
+                                      "아니에요!",
+                                      style: TextStyle(
+                                          fontSize: 30, color: kDarkFontColor),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            });
                         },
                         child: Container(
                         padding: EdgeInsets.only(bottom: 10),
