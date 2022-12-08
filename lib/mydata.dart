a3ce3e0b4a6c7544fcb5a0bf5d5b4aabc44dbbf2
7apislaz <belltech0210@gmail.com>
2022-12-08 11:51:06 +0900

    [Update] 포스트할 값 구현

--- lib/mydata.dart
+++ lib/mydata.dart
@@ -1,21 +1,29 @@
 class MyData {
   String? emotion;
   String? playing;
+  String? kidHeight;
+  String? kidWeight;
 
-  MyData(
-      {this.emotion,
-      this.playing,
-    });
+  MyData({
+    this.emotion,
+    this.playing,
+    this.kidHeight,
+    this.kidWeight,
+  });
 
   MyData.fromJson(Map<String, dynamic> json) {
     emotion = json['emotion'];
     playing = json['playing'];
+    kidHeight = json['kidHeight'];
+    kidWeight = json['kidWeight'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['emotion'] = this.emotion;
     data['playing'] = this.playing;
+    data['kidHeight'] = this.kidHeight;
+    data['kidWeight'] = this.kidWeight;
 
     return data;
   }
