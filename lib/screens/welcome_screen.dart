2fa723d0bd6f6de9d1541c11cf8864ac8bfec625
7apislaz <belltech0210@gmail.com>
2022-12-09 12:10:08 +0900

    [Update] 값 받아오는 곳 까지 커밋

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,15 +2,13 @@ import 'dart:typed_data';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:usb_serial/transaction.dart';
-import 'package:usb_serial/usb_serial.dart';
 import 'package:vision21tech_smartkiosk/constants.dart';
 import 'package:vision21tech_smartkiosk/module/button.dart';
 import 'package:vision21tech_smartkiosk/screens/emotion_kid_list_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/kid_list_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/kiosk_setting_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/measuring_screen.dart';
-
+import 'package:usb_serial/usb_serial.dart';
 import 'measurement_error.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -21,10 +19,9 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+
   @override
   int touchCount = 0;
-
-  get p => null;
 
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -81,32 +78,28 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                         buttonColor: kOrangeButtonColor,
                         textStyle: Theme.of(context).textTheme.bodyText1,
                         onPressed: () async {
-                          List<UsbDevice> devices =
-                              await UsbSerial.listDevices();
+                          List<UsbDevice> devices = await UsbSerial.listDevices();
                           print(devices);
                           UsbPort? port;
                           if (devices.length == 0) {
-                            Get.to(() => MesureErrorScreen());
+                            return;
                           }
                           port = await devices[0].create();
+
                           bool? openResult = await port?.open();
-                          if (!openResult!) {
+                          if ( !openResult! ) {
                             print("Failed to open");
                             return;
                           }
-                          await port?.setDTR(true);
-                          await port?.setFlowControl(1);
-                          await port?.setRTS(true);
-                          port?.setPortParameters(19600, UsbPort.DATABITS_8,
-                              UsbPort.STOPBITS_1_5, UsbPort.PARITY_NONE);
-                          // print first result and close port.
 
-                          Future.delayed(const Duration(milliseconds: 700), () {
-                            port?.inputStream?.listen((Uint8List event) {
-                              print(event);
-                              print(event.length);
-                            });
-                            //print(ascii.decode(event));
+                          await port?.setDTR(true);
+                          await port?.setRTS(true);
+
+                          port?.setPortParameters(19600, UsbPort.DATABITS_8,
+                              UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+                          port?.inputStream?.listen((Uint8List event) {
+                            print(event);
                           });
 
                           Get.to(() => MesuringScreen());
