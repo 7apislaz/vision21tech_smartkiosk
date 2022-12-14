4f493d264e1527afe242e29b512228ee84393ec3
7apislaz <belltech0210@gmail.com>
2022-12-14 16:51:02 +0900

    [Update] 아이 감정수집 Post 구현

--- lib/model/apikidlist_providers.dart
+++ lib/model/apikidlist_providers.dart
@@ -1,20 +1,42 @@
 import 'dart:convert';
 import 'package:get/get.dart';
-import 'package:vision21tech_smartkiosk/apikidlist.dart';
 import 'package:http/http.dart' as http;
+import 'package:vision21tech_smartkiosk/data/post_emotion.dart';
 import 'package:vision21tech_smartkiosk/screens/network_error.dart';
+import 'package:vision21tech_smartkiosk/screens/welcome_screen.dart';
 
-class ApiKidListProviders{
-  Future<KidsList> getKids() async {
-    var url = 'http://192.168.219.102:8000/kindergarten/kids';
-    final response = await http.get(Uri.parse(url), headers: {
-      'Authorization': 'Api-Key GJFQ0dMp.egxBIMx8UDCatVMObiBvqV7PK0dBABQl'})
-        .timeout(Duration(seconds: 5));
-    if (response.statusCode == 200) {
-      print(json.decode(utf8.decode(response.bodyBytes)));
-        return KidsList.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+class PostKidEmotions{
+  PostKidEmotions(
+      {
+        required this.Pemotion,
+        required this.Pselected_play,
+        required this.Pkey,});
+
+  final String Pemotion;
+  final String Pselected_play;
+  final String Pkey;
+
+  postEmotionAll() async {
+    var body = json.encode(EmotionData(
+      emotion: Pemotion,
+      selected_play: Pselected_play,
+      key: Pkey,
+    ));
+    try {
+      var url = 'http://192.168.219.102:8000/check/emotions';
+      var response = await http.post(Uri.parse(url),
+          headers: <String, String> {
+          'Content-Type': 'application/json',
+          'Authorization': 'Api-Key GJFQ0dMp.egxBIMx8UDCatVMObiBvqV7PK0dBABQl'},
+          body: body)
+          .timeout(Duration(seconds: 5));
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        print(response.body);
+        Get.off(() => WelcomeScreen());
       } else {
-      throw Exception(Get.to(() => NetworkErrorScreen()));
-    }
+        print(response.body);
+        Get.to(() => NetworkErrorScreen());
+      }
+    } catch (_) {}
   }
 }
