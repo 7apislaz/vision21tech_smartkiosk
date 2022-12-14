6a134e99bc5f50bad4072625b8f1c9bfabfe1066
7apislaz <belltech0210@gmail.com>
2022-12-14 16:56:09 +0900

    [Update] 측정페이지 KidList 구현

--- lib/screens/kid_list_screen.dart
+++ lib/screens/kid_list_screen.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:vision21tech_smartkiosk/constants.dart';
 import 'package:vision21tech_smartkiosk/screens/height_measurement_screen.dart';
 import 'package:vision21tech_smartkiosk/screens/welcome_screen.dart';
+import 'package:vision21tech_smartkiosk/model/kid_list_api.dart';
 
 class KidListScreen extends StatefulWidget {
   static String routeName = "/kidlist";
@@ -12,9 +13,25 @@ class KidListScreen extends StatefulWidget {
 }
 
 class _KidListScreenState extends State<KidListScreen> {
-  final List<String> _items = List.generate(30, (index) => 'Item ${index + 1}');
+  var kidName = '';
+  var kidPic = '';
+  List kidsLists = [];
+  bool isLoading = true;
+  KidListApi apiKidList = KidListApi();
+  Future initKids() async {
+    kidsLists = await apiKidList.getAll();
+  }
 
-  var kidName = "";
+  @override
+  void initState() {
+    super.initState();
+    initKids().then((_) {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -35,7 +52,8 @@ class _KidListScreenState extends State<KidListScreen> {
               },
             ),
           ),
-          body: SafeArea(
+          body: isLoading ? Center(child: const CircularProgressIndicator(),):
+          SafeArea(
             minimum: EdgeInsets.only(top: 60, bottom: 60, right: 60, left: 60),
             child: GestureDetector(
               onTap: () => FocusScope.of(context).unfocus(),
@@ -46,107 +64,108 @@ class _KidListScreenState extends State<KidListScreen> {
                   crossAxisSpacing: 6,
                   childAspectRatio: 1 / 1.5,
                 ),
-                itemCount: _items.length,
+                itemCount: kidsLists.length,
                 itemBuilder: (context, index) => Card(
                   margin: const EdgeInsets.all(6),
                   elevation: 4,
                   child: GridTile(
                     footer: GridTileBar(
                       backgroundColor: kOrangeButtonColor,
-                      title: Text('아이 ${_items[index].split(' ')[1]}',
+                      title: Text(kidsLists[index]["name"]!,
                         style: TextStyle(
-                          fontSize: 20,
-                          color: kDarkFontColor,
-                          fontFamily: 'Godo',
-                          fontWeight: FontWeight.normal),
-                          textAlign: TextAlign.center,),
+                            fontSize: 20,
+                            color: kDarkFontColor,
+                            fontFamily: 'Godo',
+                            fontWeight: FontWeight.normal),
+                        textAlign: TextAlign.center,),
                     ),
                     child: Center(
                       child: InkWell(
                         onTap: () {
-                          kidName = _items[index].split(' ')[1];
+                          kidName = kidsLists[index]["name"]!;
+                          kidPic = kidsLists[index]["pic"]!;
                           showDialog(
-                            context: context,
-                            barrierDismissible: false,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                titlePadding: EdgeInsets.only(
-                                    top: 100, bottom: 60, right: 60, left: 60),
-                                contentPadding:
-                                EdgeInsets.only(right: 60, left: 60),
-                                actionsPadding: EdgeInsets.only(
-                                    top: 60, bottom: 100, right: 60, left: 60),
-                                actionsAlignment: MainAxisAlignment.center,
-                                title: Text('아이 $kidName 친구야 안녕!',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontFamily: 'Godo',
-                                      fontWeight: FontWeight.normal,
-                                      fontSize: 50,
-                                      color: kDarkFontColor,
-                                    )),
-                                content: SingleChildScrollView(
-                                  child: InkWell(
-                                    child: Container(
-                                      padding: EdgeInsets.only(bottom: 10),
-                                      alignment:
-                                      AlignmentDirectional.bottomCenter,
-                                      decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                          fit: BoxFit.contain,
-                                          image: AssetImage(
-                                              'assets/images/KioskAndKid.png'),
-                                        ),
-                                      ),
-                                      width: 400,
-                                      height: 400,
-                                    ),
-                                  ),
-                                ),
-                                actions: [
-                                  ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: kOrangeButtonColor,
-                                      maximumSize: Size(260, 100),
-                                      minimumSize: Size(260, 100),
-                                    ),
-                                    onPressed: () {
-                                      Get.to(() => HeightMeasure());
-                                    },
-                                    child: Text(
-                                      "맞아요!",
-                                      style: TextStyle(fontSize: 30),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 40,
-                                  ),
-                                  ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: kGrayButtonColor,
-                                      maximumSize: Size(260, 100),
-                                      minimumSize: Size(260, 100),
-                                    ),
-                                    onPressed: () {
-                                      Get.back();
-                                    },
-                                    child: Text(
-                                      "아니에요!",
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  titlePadding: EdgeInsets.only(
+                                      top: 100, bottom: 60, right: 60, left: 60),
+                                  contentPadding:
+                                  EdgeInsets.only(right: 60, left: 60),
+                                  actionsPadding: EdgeInsets.only(
+                                      top: 60, bottom: 100, right: 60, left: 60),
+                                  actionsAlignment: MainAxisAlignment.center,
+                                  title: Text('$kidName 친구야 안녕!',
+                                      textAlign: TextAlign.center,
                                       style: TextStyle(
-                                          fontSize: 30, color: kDarkFontColor),
+                                        fontFamily: 'Godo',
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 50,
+                                        color: kDarkFontColor,
+                                      )),
+                                  content: SingleChildScrollView(
+                                    child: InkWell(
+                                      child: Container(
+                                        padding: EdgeInsets.only(bottom: 10),
+                                        alignment:
+                                        AlignmentDirectional.bottomCenter,
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            fit: BoxFit.contain,
+                                            image: NetworkImage(kidPic),
+                                          ),
+                                        ),
+                                        width: 400,
+                                        height: 400,
+                                      ),
                                     ),
                                   ),
-                                ],
-                              );
-                            });
+                                  actions: [
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: kOrangeButtonColor,
+                                        maximumSize: Size(260, 100),
+                                        minimumSize: Size(260, 100),
+                                      ),
+                                      onPressed: () {
+                                        Get.to(() => HeightMeasure());
+                                      },
+                                      child: Text(
+                                        "맞아요!",
+                                        style: TextStyle(fontSize: 30),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 40,
+                                    ),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: kGrayButtonColor,
+                                        maximumSize: Size(260, 100),
+                                        minimumSize: Size(260, 100),
+                                      ),
+                                      onPressed: () {
+                                        Get.back();
+                                      },
+                                      child: Text(
+                                        "아니에요!",
+                                        style: TextStyle(
+                                            fontSize: 30, color: kDarkFontColor),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              });
                         },
                         child: Container(
-                        padding: EdgeInsets.only(bottom: 10),
-                        alignment: AlignmentDirectional.center,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                          fit: BoxFit.contain,
-                          image: AssetImage('assets/images/KioskAndKid.png'),
+                          padding: EdgeInsets.only(bottom: 10),
+                          alignment: AlignmentDirectional.center,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.contain,
+                              image: NetworkImage(kidsLists[index]["pic"]),
+                            ),
                           ),
                         ),
                       ),
@@ -156,7 +175,6 @@ class _KidListScreenState extends State<KidListScreen> {
               ),
             ),
           ),
-        ),
         ),
       ),
     );
