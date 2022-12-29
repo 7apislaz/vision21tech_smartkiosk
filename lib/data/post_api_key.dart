eb6fa14ffd17bb729d6382fbba09e7186373df59
7apislaz <belltech0210@gmail.com>
2022-12-29 12:41:01 +0900

    [Update] api-key post Json parsing file

---
+++ lib/data/post_api_key.dart
@@ -0,0 +1,17 @@
+class ApiKeyName {
+  String? name;
+
+  ApiKeyName({
+    this.name,
+  });
+
+  ApiKeyName.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    return data;
+  }
+}
