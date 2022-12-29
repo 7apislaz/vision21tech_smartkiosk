8bf05e2d11e8d429bcef8210640a875eec016cbf
7apislaz <belltech0210@gmail.com>
2022-12-29 12:35:48 +0900

    [Update] key 값, url 암호화 스토리지 관리

--- lib/model/apikidlist_providers.dart
+++ lib/model/apikidlist_providers.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:vision21tech_smartkiosk/data/post_emotion.dart';
 import 'package:vision21tech_smartkiosk/screens/network_error.dart';
 import 'package:vision21tech_smartkiosk/screens/welcome_screen.dart';
 
 class PostKidEmotions{
+  GetStorage storage = GetStorage();
   PostKidEmotions(
       {
         required this.Pemotion,
@@ -23,11 +25,11 @@ class PostKidEmotions{
       key: Pkey,
     ));
     try {
-      var url = 'http://192.168.219.7:8000/check/emotions';
+      var url = '${storage.read('url')}/check/emotions';
       var response = await http.post(Uri.parse(url),
           headers: <String, String> {
           'Content-Type': 'application/json',
-          'Authorization': 'Api-Key 1FM5Gpa7.uJ74YSuFxXE7dVJVTekvzlcSOKsnFwvi'},
+          'Authorization': 'Api-Key ${storage.read('apikey')}'},
           body: body)
           .timeout(Duration(seconds: 5));
       if (response.statusCode == 200 || response.statusCode == 201) {
--- lib/model/kid_list_api.dart
+++ lib/model/kid_list_api.dart
@@ -1,16 +1,20 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
+import '../constants.dart';
 import '../screens/network_error.dart';
 
 class KidListApi {
+  GetStorage storage = GetStorage();
 //your http client can be http, http.Client, dio, just bring your methods here and they will work normally :D
   //a quick example, here we are retrieving all posts made available by api(100)
   getAll() async {
     try {
-      var url = 'http://192.168.219.7:8000/kindergarten/kids';
+      var url = '${storage.read('url')}/kindergarten/kids';
       var response = await http.get(Uri.parse(url), headers: {
-        'Authorization': 'Api-Key 1FM5Gpa7.uJ74YSuFxXE7dVJVTekvzlcSOKsnFwvi'})
+        'Authorization': 'Api-Key ${storage.read('apikey')}'})
           .timeout(Duration(seconds: 7));
       if (response.statusCode == 200) {
         List kidList = jsonDecode(utf8.decode(response.bodyBytes));
@@ -21,6 +25,145 @@ class KidListApi {
       }
     } catch (_) {
       Get.to(() => NetworkErrorScreen());
+    }
+  }
+  getMigrate() async {
+    try {
+      var url = '${storage.read('url')}/kindergarten/migrate';
+      var response = await http.get(Uri.parse(url), headers: {
+        'Authorization': 'Token ${storage.read('Token')}'})
+          .timeout(Duration(seconds: 7));
+      if (response.statusCode == 200) {
+        return AlertDialog(
+            titlePadding:
+            EdgeInsets.only(top: 30, bottom: 30, right: 30, left: 30),
+            contentPadding: EdgeInsets.only(right: 30, left: 30),
+            actionsPadding:
+            EdgeInsets.only(top: 30, bottom: 30, right: 30, left: 30),
+            title: Text(
+              "Success",
+              style: TextStyle(
+                fontFamily: 'Godo',
+                fontWeight: FontWeight.normal,
+                fontSize: 20,
+                color: kDarkFontColor,
+              ),
+            ),
+            content: Text(
+              "데이터 마이그레이션에 성공하였습니다.",
+              style: TextStyle(
+                fontFamily: 'Godo',
+                fontWeight: FontWeight.normal,
+                fontSize: 20,
+                color: kDarkFontColor,
+              ),
+            ),
+            actions: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kOrangeButtonColor,
+                  maximumSize: Size(100, 60),
+                  minimumSize: Size(100, 60),
+                ),
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text(
+                  "확인",
+                  style: TextStyle(
+                    color: kDarkFontColor,
+                  ),
+                ),
+              ),
+            ]);
+      } else {
+        return AlertDialog(
+            titlePadding:
+            EdgeInsets.only(top: 30, bottom: 30, right: 30, left: 30),
+            contentPadding: EdgeInsets.only(right: 30, left: 30),
+            actionsPadding:
+            EdgeInsets.only(top: 30, bottom: 30, right: 30, left: 30),
+            title: Text(
+              "Error",
+              style: TextStyle(
+                fontFamily: 'Godo',
+                fontWeight: FontWeight.normal,
+                fontSize: 20,
+                color: kDarkFontColor,
+              ),
+            ),
+            content: Text(
+              "데이터 마이그레이션에 실패하였습니다.\n오류내용:${response.body}",
+              style: TextStyle(
+                fontFamily: 'Godo',
+                fontWeight: FontWeight.normal,
+                fontSize: 20,
+                color: kDarkFontColor,
+              ),
+            ),
+            actions: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kOrangeButtonColor,
+                  maximumSize: Size(100, 60),
+                  minimumSize: Size(100, 60),
+                ),
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text(
+                  "확인",
+                  style: TextStyle(
+                    color: kDarkFontColor,
+                  ),
+                ),
+              ),
+            ]);
+      }
+    } catch (e) {
+      print(e);
+      return AlertDialog(
+          titlePadding:
+          EdgeInsets.only(top: 30, bottom: 30, right: 30, left: 30),
+          contentPadding: EdgeInsets.only(right: 30, left: 30),
+          actionsPadding:
+          EdgeInsets.only(top: 30, bottom: 30, right: 30, left: 30),
+          title: Text(
+            "Error",
+            style: TextStyle(
+              fontFamily: 'Godo',
+              fontWeight: FontWeight.normal,
+              fontSize: 20,
+              color: kDarkFontColor,
+            ),
+          ),
+          content: Text(
+            "데이터 마이그레이션에 실패하였습니다.\n오류내용: $e",
+            style: TextStyle(
+              fontFamily: 'Godo',
+              fontWeight: FontWeight.normal,
+              fontSize: 20,
+              color: kDarkFontColor,
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kOrangeButtonColor,
+                maximumSize: Size(100, 60),
+                minimumSize: Size(100, 60),
+              ),
+              onPressed: () {
+                Get.back();
+              },
+              child: Text(
+                "확인",
+                style: TextStyle(
+                  color: kDarkFontColor,
+                ),
+              ),
+            ),
+          ]);
     }
   }
 }
