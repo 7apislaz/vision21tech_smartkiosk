878bbf690cc8ff5bf64c4804d449842e6a079dcf
7apislaz <belltech0210@gmail.com>
2023-01-04 18:35:51 +0900

    [Update] 키측정페이지 파싱 진행중

--- lib/screens/measuring_screen.dart
+++ lib/screens/measuring_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:get/get.dart';
 import 'package:vision21tech_smartkiosk/screens/welcome_screen.dart';
 
@@ -6,15 +9,82 @@ import '../module/audio.dart';
 
 class MesuringScreen extends StatefulWidget {
   static String routeName = "/mesuring";
+  final BluetoothDevice server;
+
+  const MesuringScreen({required this.server});
 
   @override
   _MesuringScreenState createState() => _MesuringScreenState();
 }
+class _Message {
+  String text;
 
+  _Message(this.text);
+}
 class _MesuringScreenState extends State<MesuringScreen> {
   ButtonAudios buttonAudios = ButtonAudios();
+  BluetoothConnection? connection;
+
+  List<_Message> messages = List<_Message>.empty(growable: true);
+  String _messageBuffer = '';
+
+  bool isConnecting = true;
+  bool get isConnected => (connection?.isConnected ?? false);
+
+  bool isDisconnecting = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    BluetoothConnection.toAddress(widget.server.address).then((_connection) {
+      print('Connected to the device');
+      connection = _connection;
+      setState(() {
+        isConnecting = false;
+        isDisconnecting = false;
+      });
+
+      connection!.input!.listen(_onDataReceived).onDone(() {
+        // Example: Detect which side closed the connection
+        // There should be `isDisconnecting` flag to show are we are (locally)
+        // in middle of disconnecting process, should be set before calling
+        // `dispose`, `finish` or `close`, which all causes to disconnect.
+        // If we except the disconnection, `onDone` should be fired as result.
+        // If we didn't except this (no flag set), it means closing by remote.
+        if (isDisconnecting) {
+          print('Disconnecting locally!');
+        } else {
+          print('Disconnected remotely!');
+        }
+        if (this.mounted) {
+          setState(() {});
+        }
+      });
+    }).catchError((error) {
+      print('Cannot connect, exception occured');
+      print(error);
+    });
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and disconnect
+    if (isConnected) {
+      isDisconnecting = true;
+      connection?.dispose();
+      connection = null;
+    }
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final list = messages.map((_message) {
+                    (_message.text.trim());
+    }).toList();
+
     return WillPopScope(
         onWillPop: () async => false,
     child: Container(
@@ -59,6 +129,8 @@ class _MesuringScreenState extends State<MesuringScreen> {
                     SizedBox(height: 20),
                     Text("끝날 때까지 기다려 주세요.", textScaleFactor: 3.5,),
                     SizedBox(height: 20),
+                    Text("$list", textScaleFactor: 3.5,),
+                    SizedBox(height: 20),
                   ],
                 ),
               ),
@@ -67,5 +139,54 @@ class _MesuringScreenState extends State<MesuringScreen> {
       ),
     ),
     );
+  }
+  void _onDataReceived(Uint8List data) {
+    // Allocate buffer for parsed data
+    int backspacesCounter = 0;
+    data.forEach((byte) {
+      if (byte == 8 || byte == 127) {
+        backspacesCounter++;
+      }
+    });
+    Uint8List buffer = Uint8List(data.length - backspacesCounter);
+    int bufferIndex = buffer.length;
+
+    // Apply backspace control character
+    backspacesCounter = 0;
+    for (int i = data.length - 1; i >= 0; i--) {
+      if (data[i] == 8 || data[i] == 127) {
+        backspacesCounter++;
+      } else {
+        if (backspacesCounter > 0) {
+          backspacesCounter--;
+        } else {
+          buffer[--bufferIndex] = data[i];
+        }
+      }
+    }
+
+    // Create message if there is new line character
+    String dataString = String.fromCharCodes(buffer);
+    int index = buffer.indexOf(13);
+    if (~index != 0) {
+      setState(() {
+        messages.add(
+          _Message(
+            backspacesCounter > 0
+                ? _messageBuffer.substring(
+                0, _messageBuffer.length - backspacesCounter)
+                : _messageBuffer + dataString.substring(0, index),
+          ),
+        );
+        _messageBuffer = dataString.substring(index);
+        print(_messageBuffer);
+      });
+    } else {
+      _messageBuffer = (backspacesCounter > 0
+          ? _messageBuffer.substring(
+          0, _messageBuffer.length - backspacesCounter)
+          : _messageBuffer + dataString);
+      print(_messageBuffer);
+    }
   }
 }
